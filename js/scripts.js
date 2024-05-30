3182ae0ce5fdb353a784c63bdfb325b0a381d135
Nzinga Nuru <nzinga.nuru@gmail.com>
2024-05-30 16:49:35 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Carregar menu
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('menu-container').innerHTML = data;
+            // Adicionar classe 'active' ao link atual
+            var menuLinks = document.querySelectorAll('.menu-link');
+            menuLinks.forEach(function(link) {
+                if (link.href === window.location.href) {
+                    link.classList.add('active');
+                }
+            });
+        })
+        .catch(error => console.error('Erro ao carregar o menu:', error));
+
+    // Carregar footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o footer:', error));
+});
+
+
+
